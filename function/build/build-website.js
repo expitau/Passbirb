77fddca490c7812a2e8caa2eb803f67f10100c1e
expitau-dev <expitau@gmail.com>
2023-01-15 13:05:24 -0500

    Add salt history

--- function/build/build-website.js
+++ function/build/build-website.js
@@ -4,7 +4,7 @@ const sass = require('sass')
 
 require('process').chdir(path.normalize(__dirname + '/../../'))
 
-if (!fs.existsSync("website/dist")){
+if (!fs.existsSync("website/dist")) {
     fs.mkdirSync("website/dist");
 }
 
@@ -15,7 +15,7 @@ fs.writeFileSync("website/dist/bundle.js", bundle)
 fs.writeFileSync("website/dist/zxcvbn.js", zxcvbn)
 fs.readFileSync("website/src/buildManifest.csv", "utf8").split(",").forEach(file => fs.copyFileSync(`website/src/${file.trim()}`, `website/dist/${file.trim()}`))
 
-fs.readdirSync("website/src").filter(fileName => fileName.endsWith(".scss") || fileName.endsWith(".sass") ).forEach(sassFile => {
-    fs.writeFileSync(`website/dist/${sassFile.replace(/\.s[ca]ss$/,'')}.css`,sass.compile(`website/src/${sassFile}`).css)
+fs.readdirSync("website/src").filter(fileName => !fileName.startsWith("_") && (fileName.endsWith(".scss") || fileName.endsWith(".sass"))).forEach(sassFile => {
+    fs.writeFileSync(`website/dist/${sassFile.replace(/\..*$/, '')}.css`, sass.compile(`website/src/${sassFile}`).css)
     // fs.writeFileSync(`website/dist/${sassFile.replace(/\.s[ca]ss$/,'')}.css`,sass.compile(`website/src/${sassFile}`,{style: "compressed"}).css)
 })
--- website/src/app.js
+++ website/src/app.js
@@ -1,3 +1,4 @@
+const VERSION_ID = "0.4.1"
 
 // Load VueJS
 const { createApp } = Vue
@@ -14,17 +15,35 @@ function loadFile(filePath) {
     return result;
 }
 
+
+function saveLocalStorage(state) {
+    localStorage.setItem('state', JSON.stringify({ ...state, version: VERSION_ID }))
+}
+
+function loadLocalStorage() {
+    state = JSON.parse(localStorage.getItem('state'))
+    if (state?.version == VERSION_ID) {
+        return state
+    } else {
+        localStorage.clear()
+        return null
+    }
+}
+
 // Service to compute hashes in non-blocking thread
 let backgroundServiceWorker;
 
 let app = createApp({
     data() {
         return {
-            masterPassword: "", // 
+            masterPassword: "",
             passwordVisible: true,
+            saltHistoryVisible: false,
+            saltHistory: [],
             salt: "",
             hashedPassword: "",
-            darkMode: window.matchMedia("(prefers-color-scheme: dark)").matches
+            darkMode: window.matchMedia("(prefers-color-scheme: dark)").matches,
+            ...loadLocalStorage()
         }
     },
     methods: {
@@ -42,15 +61,51 @@ let app = createApp({
                 generatePassword(this.masterPassword, this.salt).then(res => this.hashedPassword = res)
             }
         },
-        copyText: (text) => {
+        saveSalt() {
+            this.saltHistory.unshift(this.salt);
+            this.saltHistory = this.saltHistory.filter((c, index) => {
+                return this.saltHistory.indexOf(c) == index && c != ""
+            })
+            this.saveAppState()
+        },
+        copyText(text) {
             navigator.clipboard.writeText(text)
             showMessage('Copied to clipboard!')
         },
-        showMessage
+        getAppState() {
+            return {
+                saltHistory: this.saltHistory
+            }
+        },
+        onDropdownUnfocused(e) {
+            this.saveSalt();
+            if (e?.relatedTarget && Array.from(document.getElementsByClassName('dropdown')).some(x => x.contains(e.relatedTarget))) {
+                return
+            }
+            this.saltHistoryVisible = false
+        },
+        saveAppState(state) {
+            return saveLocalStorage(state || this.getAppState())
+        }
     },
     computed: {
         passwordEntropy() {
             return zxcvbn(this.masterPassword).guesses_log10
+        },
+        historySearchResults() {
+            return this.saltHistory
+                .map((value, idx) => ({ value, idx }))
+                .filter(x => x.value.toLowerCase().includes(this.salt.toLowerCase()))
+                .sort((a, b) => {
+                    if (a.value.toLowerCase().indexOf(this.salt.toLowerCase()) > b.value.toLowerCase().indexOf(this.salt.toLowerCase())) {
+                        return 1;
+                    } else if (a.value.toLowerCase().indexOf(this.salt.toLowerCase()) < b.value.toLowerCase().indexOf(this.salt.toLowerCase())) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                })
+            // .slice(0, 5)
         }
     }
 })
@@ -64,12 +119,12 @@ if (window.Worker) {
 
 
 function showMessage(message) {
-    console.log("run")
     let el = document.getElementById('messageToast')
     el.innerHTML = message
     el.classList.remove('fade-in-out')
     window.requestAnimationFrame(() => { el.classList.add('fade-in-out') })
 }
+
 
 function loadFile(filePath) {
     var result = null;
